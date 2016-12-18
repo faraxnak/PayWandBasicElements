37a76da4102d841982d247abc7a0dfdd3e0e1fc8
faraxnak <farid@farids-macbook-pro.local>
2016-12-18 11:30:39 +0330

    Initial Commit

---
+++ PayWandBasicElements/PayWandBasicElements.h
@@ -0,0 +1,19 @@
+//
+//  PayWandBasicElements.h
+//  PayWandBasicElements
+//
+//  Created by Farid on 12/18/16.
+//  Copyright © 2016 ParsPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PayWandBasicElements.
+FOUNDATION_EXPORT double PayWandBasicElementsVersionNumber;
+
+//! Project version string for PayWandBasicElements.
+FOUNDATION_EXPORT const unsigned char PayWandBasicElementsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayWandBasicElements/PublicHeader.h>
+
+
