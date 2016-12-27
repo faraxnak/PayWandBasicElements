e2c33efddbb6421ea2e4cb5a8acc842f1f777a01
faraxnak <farid@Farids-MacBook-Pro.local>
2016-12-27 19:28:00 +0330

    extensions added

--- PayWandBasicElements/Extensions.swift
+++ PayWandBasicElements/Extensions.swift
@@ -199,3 +199,196 @@ public struct DeviceType
     static let IS_IPHONE_6P         = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_LENGTH == 736.0
     static let IS_IPAD              = UIDevice.current.userInterfaceIdiom == .pad || ScreenSize.SCREEN_MAX_LENGTH == 1024.0
 }
+
+
+extension UIImage {
+    public func imageWithSize(_ size:CGSize) -> UIImage
+    {
+        var scaledImageRect = CGRect.zero;
+        
+        let aspectWidth:CGFloat = size.width / self.size.width;
+        let aspectHeight:CGFloat = size.height / self.size.height;
+        let aspectRatio:CGFloat = min(aspectWidth, aspectHeight);
+        
+        scaledImageRect.size.width = self.size.width * aspectRatio;
+        scaledImageRect.size.height = self.size.height * aspectRatio;
+        scaledImageRect.origin.x = (size.width - scaledImageRect.size.width) / 2.0;
+        scaledImageRect.origin.y = (size.height - scaledImageRect.size.height) / 2.0;
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0);
+        
+        self.draw(in: scaledImageRect);
+        
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return scaledImage!;
+    }
+}
+
+extension CGSize {
+    public func scale(_ factor : CGFloat) -> CGSize{
+        return CGSize(width: self.width * factor, height: self.height * factor)
+    }
+}
+
+///
+
+//
+//  CAAnimation+Closure.swift
+//  CAAnimation+Closures
+//
+//  Created by Honghao Zhang on 2/5/15.
+//  Copyright (c) 2015 Honghao Zhang. All rights reserved.
+//
+
+import QuartzCore
+
+/// CAAnimation Delegation class implementation
+class CAAnimationDelegateModified: NSObject, CAAnimationDelegate {
+    /// start: A block (closure) object to be executed when the animation starts. This block has no return value and takes no argument.
+    var start: (() -> Void)?
+    
+    /// completion: A block (closure) object to be executed when the animation ends. This block has no return value and takes a single Boolean argument that indicates whether or not the animations actually finished.
+    var completion: ((Bool) -> Void)?
+    
+    /// startTime: animation start date
+    fileprivate var startTime: Date!
+    fileprivate var animationDuration: TimeInterval!
+    fileprivate var animatingTimer: Timer!
+    
+    /// animating: A block (closure) object to be executed when the animation is animating. This block has no return value and takes a single CGFloat argument that indicates the progress of the animation (From 0 ..< 1)
+    var animating: ((CGFloat) -> Void)? {
+        willSet {
+            if animatingTimer == nil {
+                animatingTimer = Timer(timeInterval: 0, target: self, selector: #selector(CAAnimationDelegateModified.animationIsAnimating(_:)), userInfo: nil, repeats: true)
+            }
+        }
+    }
+    
+    /**
+     Called when the animation begins its active duration.
+     
+     - parameter theAnimation: the animation about to start
+     */
+    func animationDidStart(_ theAnimation: CAAnimation) {
+        start?()
+        if animating != nil {
+            animationDuration = theAnimation.duration
+            startTime = Date()
+            RunLoop.current.add(animatingTimer, forMode: RunLoopMode.defaultRunLoopMode)
+        }
+    }
+    
+    /**
+     Called when the animation completes its active duration or is removed from the object it is attached to.
+     
+     - parameter theAnimation: the animation about to end
+     - parameter finished:     A Boolean value indicates whether or not the animations actually finished.
+     */
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        completion?(flag)
+        animatingTimer?.invalidate()
+    }
+    
+    /**
+     Called when the animation is executing
+     
+     - parameter timer: timer
+     */
+    func animationIsAnimating(_ timer: Timer) {
+        let progress = CGFloat(Date().timeIntervalSince(startTime) / animationDuration)
+        if progress <= 1.0 {
+            animating?(progress)
+        }
+    }
+}
+
+public extension CAAnimation {
+    /// A block (closure) object to be executed when the animation starts. This block has no return value and takes no argument.
+    public var start: (() -> Void)? {
+        set {
+            if let animationDelegate = delegate as? CAAnimationDelegateModified {
+                animationDelegate.start = newValue
+            } else {
+                let animationDelegate = CAAnimationDelegateModified()
+                animationDelegate.start = newValue
+                delegate = animationDelegate
+            }
+        }
+        
+        get {
+            if let animationDelegate = delegate as? CAAnimationDelegateModified {
+                return animationDelegate.start
+            }
+            
+            return nil
+        }
+    }
+    
+    /// A block (closure) object to be executed when the animation ends. This block has no return value and takes a single Boolean argument that indicates whether or not the animations actually finished.
+    public var completion: ((Bool) -> Void)? {
+        set {
+            if let animationDelegate = delegate as? CAAnimationDelegateModified {
+                animationDelegate.completion = newValue
+            } else {
+                let animationDelegate = CAAnimationDelegateModified()
+                animationDelegate.completion = newValue
+                delegate = animationDelegate
+            }
+        }
+        
+        get {
+            if let animationDelegate = delegate as? CAAnimationDelegateModified {
+                return animationDelegate.completion
+            }
+            
+            return nil
+        }
+    }
+    
+    /// A block (closure) object to be executed when the animation is animating. This block has no return value and takes a single CGFloat argument that indicates the progress of the animation (From 0 ..< 1)
+    public var animating: ((CGFloat) -> Void)? {
+        set {
+            if let animationDelegate = delegate as? CAAnimationDelegateModified {
+                animationDelegate.animating = newValue
+            } else {
+                let animationDelegate = CAAnimationDelegateModified()
+                animationDelegate.animating = newValue
+                delegate = animationDelegate
+            }
+        }
+        
+        get {
+            if let animationDelegate = delegate as? CAAnimationDelegateModified {
+                return animationDelegate.animating
+            }
+            
+            return nil
+        }
+    }
+    
+    /// Alias to `animating`
+    public var progress: ((CGFloat) -> Void)? {
+        set {
+            animating = newValue
+        }
+        
+        get {
+            return animating
+        }
+    }
+}
+
+public extension CALayer {
+    /**
+     Add the specified animation object to the layer’s render tree. Could provide a completion closure.
+     
+     - parameter anim:       The animation to be added to the render tree. This object is copied by the render tree, not referenced. Therefore, subsequent modifications to the object are not propagated into the render tree.
+     - parameter key:        A string that identifies the animation. Only one animation per unique key is added to the layer. The special key kCATransition is automatically used for transition animations. You may specify nil for this parameter.
+     - parameter completion: A block object to be executed when the animation ends. This block has no return value and takes a single Boolean argument that indicates whether or not the animations actually finished before the completion handler was called. Default value is nil.
+     */
+    func addAnimation(_ anim: CAAnimation, forKey key: String?, withCompletion completion: ((Bool) -> Void)?) {
+        anim.completion = completion
+        add(anim, forKey: key)
+    }
+}
